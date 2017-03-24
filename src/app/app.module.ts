e8dc19644ac25bd6a86e8a66650194b2b7616e05
Marcio Luiz <Marcio Luiz>
2017-03-23 23:22:00 -0300

    Aula: Criação de módulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { CursosModule } from './cursos/cursos.module';
+
 import { AppComponent } from './app.component';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
@@ -15,7 +17,8 @@ import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
